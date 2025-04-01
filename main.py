1f14eaab1851404a043b6c86d8310b9073094f24
kiwi08522 <ryanbarnard180@gmail.com>
2025-04-01 00:45:53 -0500

    Added error handling for user inputs.

--- main.py
+++ main.py
@@ -13,13 +13,29 @@
 '''
 
 def get_user_input():
-    stock_symbol = input("Please enter the stock symbol you are looking for (e.g. GOOG, AAPL): ").upper()
+    # Prompt user for stock symbol, make sure it's not blank.
+    stock_symbol = input("Please enter the stock symbol you are looking for (e.g. GOOG, AAPL): ").upper().strip()
+    while not stock_symbol:
+        print("The stock symbol cannot be blank.")
+        stock_symbol = input("Please enter the stock symbol you are looking for (e.g. GOOG, AAPL): ").upper().strip()
+
+    # Prompt user for chart type, either 1 or 2.
     print("Chart Types\n---------------\n1. Bar\n2. Line")
     chart_type = input("Please enter the chart you'd like (1, 2): ")
+    while chart_type not in ["1", "2"]:
+        print("Please enter 1 or 2.")
+        chart_type = input("Please enter the chart you'd like (1, 2): ")
+
+    # Prompt user for time series, either 1, 2, 3, or 4.
     print("Please select the time series of the chart you want to generate")
     print("------------------------------------------------------")
     print("1. Intraday\n2. Daily\n3. Weekly\n4. Monthly")
     time_series = input("Enter time series option (1, 2, 3, 4): ")
+    while time_series not in ["1", "2", "3", "4"]:
+        print("Please enter 1, 2, 3, or 4.")
+        time_series = input("Enter time series option (1, 2, 3, 4): ")
+
+    # Prompt user for both start date and end date.
     start_date = input("Please enter the start date (YYYY-MM-DD): ")
     end_date = input("Please enter the end date (YYYY-MM-DD): ")
 
@@ -28,8 +44,11 @@ def get_user_input():
         print("Error: End date cannot be before start date.")
         end_date = input("Please enter the end date (YYYY-MM-DD): ")
     
+    return stock_symbol, chart_type, time_series, start_date, end_date
+    
 def main():
-    get_user_input()
+    user_data = get_user_input()
+    print(user_data)
 
 if __name__ == "__main__":
     main()
